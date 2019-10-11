5db87693ded28cf2dd872f22e999bbbe3eba46c1
FL-MAY <yangjian@roborock.com>
2019-10-11 20:13:30 +0800

    init commit

--- Roboscrub_ws/build/CleaningCar/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yangjian/AWS_CodeBuild/AWS_CodeBuild/Roboscrub_ws/src/CleaningCar
-# Build directory: /home/yangjian/AWS_CodeBuild/AWS_CodeBuild/Roboscrub_ws/build/CleaningCar
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- Roboscrub_ws/build/scrubber_gazebo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yangjian/AWS_CodeBuild/AWS_CodeBuild/Roboscrub_ws/src/scrubber_gazebo
-# Build directory: /home/yangjian/AWS_CodeBuild/AWS_CodeBuild/Roboscrub_ws/build/scrubber_gazebo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
